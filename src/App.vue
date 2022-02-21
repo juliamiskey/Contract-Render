2a116d3bd587257e6a3d23404ffeac7dd2010dc0
Julia <juliamiskey@gmail.com>
2022-02-21 12:03:48 +0000

    adding content

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
+<!-- 
+1.The implementation for getSentencesForTextBlock in api.js is complete and calculates all sentences from a TextBlock
+2.The contract is retrieved from getContract in api.js and the contract's text is rendered as a document.
+3.While hovering on text, the surrounding sentence should be highlighted in blue.
+4.The user can select a single sentence on click, or multiple sentences on ctrl-click. The selected sentence(s) should be highlighted in red.
+5.The user can click a button to clear their selection. The button should be disabled if no sentences are selected. -->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>Contract Render</h1>
+      <TextBlock />
+      <button 
+        class="button"
+        type="button"
+        >
+          Clear section
+        </button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TextBlock from './components/TextBlock.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TextBlock
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .container {
+    margin: 0 auto;
+    max-width: 850px;
+    text-align: center;
+  }
+
+  .text-hover {
+    background: blue;
+    color: black;
+  }
+
+  .text-clicked {
+    background: red;
+    color: black;
+  }
+
+  .button {
+    background-color:grey;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 1em 0;
+  }
 </style>
